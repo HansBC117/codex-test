827110d6659076db5223a9293696eb86f13ff419
HansBC117 <HaloHans117@gmail.com>
2025-05-22 17:02:04 +0200

    Update run_analysis_B.m

--- run_analysis.m
+++ run_analysis.m
@@ -1,45 +1,59 @@
-
-% Load data
+% Load pressure data
 data = load('Exercise_A_raw_pressures.mat');
 
-% Define parameters
-encoder_resolution = 0.5; % degrees
-TDC_shift = 0.5; % degrees after max motored pressure
-atm_pressure = 1.013; % bar
-gain = 10; % bar/V
+% Define input parameters for signal processing
+encoder_resolution = 0.5; % deg
+TDC_shift = 0.5;          % deg
+atm_pressure = 1.013;     % bar
+gain = 10;                % bar/V
 
-% Call function
-[corrected_cycles, avg_pressure, rep_pressure, Theta, V_theta, P_rep] = process_cylinder_pressure( ...
-    data.motored_pressure_signal, data.fired_pressure_signal, encoder_resolution, TDC_shift, atm_pressure, gain);
+% Call Step A processing function
+[~, avg_pressure, ~, Theta, ~, ~] = process_cylinder_pressure_B( ...
+    data.motored_pressure_signal, ...
+    data.fired_pressure_signal, ...
+    encoder_resolution, ...
+    TDC_shift, ...
+    atm_pressure, ...
+    gain);
 
-% Plot results
+% Define engine and gas properties for Exercise B
+S = 114.3e-3;    % stroke [m]
+B = 82.6e-3;     % bore [m]
+L = 254e-3;      % connecting rod [m]
+CR = 7;          % compression ratio
+Tinlet = 300;    % inlet temperature [K]
+gamma = 1.3;     % heat capacity ratio
+
+% Call analysis function for Programming Exercise B
+[IMEP, PMEP, V_theta, AHRR, AHR, Tgas_mean] = ...
+    analyze_cylinder_pressure(Theta, avg_pressure, S, B, L, CR, Tinlet, gamma);
+
+% ---- PLOTS ----
+
+% Plot AHRR
 figure;
-plot(Theta, avg_pressure, 'r', 'LineWidth', 2);
-hold on;
-plot(Theta, rep_pressure, 'b', 'LineWidth', 2);
+plot(Theta, AHRR);
 xlabel('Crank Angle Degree (CAD)');
-ylabel('Pressure (bar)');
-legend('Average Cycle', 'Representative Cycle');
-title('Cylinder Pressure vs Crank Angle');
+ylabel('AHRR [J/deg]');
+title('Apparent Heat Release Rate');
 grid on;
 
-
-% Plot p-V diagram
+% Plot Cumulative Heat Release
 figure;
-plot(V_theta * 1e6, P_rep / 1e5, 'k');  % cm^3 vs bar
-xlabel('Volume [cm^3]');
-ylabel('Pressure [bar]');
-title('p-V Diagram');
+plot(Theta, AHR);
+xlabel('Crank Angle Degree (CAD)');
+ylabel('Cumulative Heat Release [J]');
+title('Apparent Heat Release');
 grid on;
 
-% Plot log-log p-V diagram
+% Plot Estimated In-Cylinder Gas Temperature
 figure;
-loglog(V_theta, P_rep, 'm');
-xlabel('Log Volume [m^3]');
-ylabel('Log Pressure [Pa]');
+plot(Theta, Tgas_mean);
+xlabel('Crank Angle Degree (CAD)');
+ylabel('Gas Temperature [K]');
+title('Estimated Mean In-Cylinder Gas Temperature');
+grid on;
 
-
-
-
-
-
+% Display IMEP and PMEP
+fprintf('IMEP: %.2f bar\n', IMEP / 1e5);
+fprintf('PMEP: %.2f bar\n', PMEP / 1e5);
